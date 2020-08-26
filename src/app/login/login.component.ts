75efdf8d49e267c531f2b46637be945a4697581e
Shah-Shahid <shahid.sheikhpora@gmail.com>
2020-08-26 15:10:24 +0530

    added spinner while logging in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 import Swal from 'sweetalert2';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
   user:any={};
   errorLbl:string;
 
-  constructor(private myService:LoginService,private route:Router) { }
+  constructor(private myService:LoginService,private route:Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     localStorage.setItem('url','login');
@@ -24,28 +25,32 @@ export class LoginComponent implements OnInit {
  
   }
   onSubmit() {
-    try {
-        this.myService.login(this.user).subscribe((response:any)=>{
-        
-          if(response.body!=null){
+    this.spinner.show();
+    this.myService.login(this.user).subscribe((response:any)=>{
+    // console.log(response.body);
+     //console.log(response.status);
+     if(response.status==200){
 
-            if(response.body['role']=="Admin")  {
-              localStorage.setItem('token',response.body['token']);
-              localStorage.setItem('role',response.body['role']);
-              localStorage.setItem('user',this.user.username);        
-              this.route.navigateByUrl('search');         
-            }
-          }
-          else {   
-              console.log('else executed');   
-              Swal.fire('oops', 'Wrong Credentials', 'error');
-            }
-        
-        }); 
-    } catch(err) {
-        console.log('error' + err);
-        Swal.fire('oops', 'Wrong Credentials', 'error');
+        if(response.body['role']=="Admin")  {
+          localStorage.setItem('token',response.body['token']);
+          localStorage.setItem('role',response.body['role']);
+          localStorage.setItem('user',this.user.username);        
+          this.route.navigateByUrl('search');  
+          this.spinner.hide();       
+        }
       }
+      else {   
+          this.spinner.hide();  
+          Swal.fire('oops', 'Wrong Credentials', 'error');
+        }
+      
+      },
+      (err:any)=>{
+        this.spinner.hide();
+        Swal.fire('oops', 'Wrong Credentials', 'error');
+    
+    });
+    this.spinner.hide();
   }
       
 }
